883c387261c7da63c80d6e07e9a5354d51b32da6
sandeep-jar <sandeepramakant@changejar.in>
2024-04-30 00:34:02 +0530

    flow start

---
+++ src/org/fog/test/perfeval/AgricultureSimulation.java
@@ -0,0 +1,137 @@
+package org.fog.test.perfeval;
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Pe;
+import org.cloudbus.cloudsim.Storage;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.power.PowerHost;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+import org.cloudbus.cloudsim.sdn.overbooking.BwProvisionerOverbooking;
+import org.cloudbus.cloudsim.sdn.overbooking.PeProvisionerOverbooking;
+import org.fog.application.AppEdge;
+import org.fog.application.AppLoop;
+import org.fog.application.Application;
+import org.fog.application.selectivity.FractionalSelectivity;
+import org.fog.entities.*;
+import org.fog.entities.MicroserviceFogDevice;
+import org.fog.entities.PlacementRequest;
+import org.fog.mobilitydata.DataParser;
+import org.fog.mobilitydata.RandomMobilityGenerator;
+import org.fog.mobilitydata.References;
+import org.fog.placement.LocationHandler;
+import org.fog.placement.MicroservicesController;
+import org.fog.placement.MicroservicesMobilityClusteringController;
+import org.fog.placement.PlacementLogicFactory;
+import org.fog.policy.AppModuleAllocationPolicy;
+import org.fog.scheduler.StreamOperatorScheduler;
+import org.fog.utils.FogLinearPowerModel;
+import org.fog.utils.FogUtils;
+import org.fog.utils.TimeKeeper;
+import org.fog.utils.distribution.DeterministicDistribution;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.*;
+
+public class AgricultureSimulation {
+    static List<FogDevice> fogDevices = new ArrayList<FogDevice>();
+    static List<Sensor> sensors = new ArrayList<Sensor>();
+    static List<Actuator> actuators = new ArrayList<Actuator>();
+
+    static LocationHandler locator;
+
+    static boolean CLOUD = false;
+
+    static double SENSOR_TRANSMISSION_TIME = 10;
+
+    public static void main(String[] args) {
+        try {
+            Log.disable();
+            int num_user = 1; // number of cloud users
+            Calendar calendar = Calendar.getInstance();
+            boolean trace_flag = false; // mean trace events
+
+            CloudSim.init(num_user, calendar, trace_flag);
+
+            String appId = "rachana_agriculture";
+            FogBroker broker = new FogBroker("broker");
+            Log.printLine("Execution Complete");
+
+            Application application =   createApplication(appId, broker.getId());
+            application.setUserId(broker.getId());
+
+
+        }
+        catch (Exception err) {
+            Log.printLine("error occurred : " + err.getLocalizedMessage());
+        }
+
+
+    }
+
+
+    private static Application createApplication(String appId, int userId) {
+
+        Application application = Application.createApplication(appId, userId);
+
+        /*
+         * Adding modules (vertices) to the application model (directed graph)
+         */
+        application.addAppModule("clientModule", 128, 150, 100);
+        application.addAppModule("mService1", 512, 250, 200);
+        application.addAppModule("mService2", 512, 350, 200);
+        application.addAppModule("mService3", 2048, 450, 1000);
+
+        /*
+         * Connecting the application modules (vertices) in the application model (directed graph) with edges
+         */
+
+        application.addAppEdge("SENSOR", "clientModule", 1000, 500, "SENSOR", Tuple.UP, AppEdge.SENSOR);
+        application.addAppEdge("clientModule", "mService1", 2000, 500, "RAW_DATA", Tuple.UP, AppEdge.MODULE);
+        application.addAppEdge("mService1", "mService2", 2500, 500, "FILTERED_DATA1", Tuple.UP, AppEdge.MODULE);
+        application.addAppEdge("mService1", "mService3", 4000, 500, "FILTERED_DATA2", Tuple.UP, AppEdge.MODULE);
+
+        application.addAppEdge("mService2", "clientModule", 14, 500, "RESULT1", Tuple.DOWN, AppEdge.MODULE);
+        application.addAppEdge("mService3", "clientModule", 28, 500, "RESULT2", Tuple.DOWN, AppEdge.MODULE);
+        application.addAppEdge("clientModule", "DISPLAY", 14, 500, "RESULT1_DISPLAY", Tuple.DOWN, AppEdge.ACTUATOR);
+        application.addAppEdge("clientModule", "DISPLAY", 14, 500, "RESULT2_DISPLAY", Tuple.DOWN, AppEdge.ACTUATOR);
+
+
+        /*
+         * Defining the input-output relationships (represented by selectivity) of the application modules.
+         */
+        application.addTupleMapping("clientModule", "SENSOR", "RAW_DATA", new FractionalSelectivity(0.9));
+        application.addTupleMapping("mService1", "RAW_DATA", "FILTERED_DATA1", new FractionalSelectivity(1.0));
+        application.addTupleMapping("mService1", "RAW_DATA", "FILTERED_DATA2", new FractionalSelectivity(1.0));
+        application.addTupleMapping("mService2", "FILTERED_DATA1", "RESULT1", new FractionalSelectivity(1.0));
+        application.addTupleMapping("mService3", "FILTERED_DATA2", "RESULT2", new FractionalSelectivity(1.0));
+        application.addTupleMapping("clientModule", "RESULT1", "RESULT1_DISPLAY", new FractionalSelectivity(1.0));
+        application.addTupleMapping("clientModule", "RESULT2", "RESULT2_DISPLAY", new FractionalSelectivity(1.0));
+
+        application.setSpecialPlacementInfo("mService3", "cloud");
+        if (CLOUD) {
+            application.setSpecialPlacementInfo("mService1", "cloud");
+            application.setSpecialPlacementInfo("mService2", "cloud");
+        }
+
+        final AppLoop loop1 = new AppLoop(new ArrayList<String>() {{
+            add("SENSOR");
+            add("clientModule");
+            add("mService1");
+            add("mService2");
+            add("clientModule");
+            add("DISPLAY");
+        }});
+
+        List<AppLoop> loops = new ArrayList<AppLoop>() {{
+            add(loop1);
+        }};
+        application.setLoops(loops);
+
+
+//        application.createDAG();
+
+        return application;
+    }
+
+}
